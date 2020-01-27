15079f06fa31723ceaaa6dfd3a4cf643e69dcd48
Alexandre Brault <abrault@mapgears.com>
2020-01-27 10:54:17 -0500

    Allow xml resampling to be used outside the repo

--- pgrastertime/processes/process.py
+++ pgrastertime/processes/process.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from collections import ChainMap
+import os
+from urllib.parse import urlparse
+
+from pgrastertime import CONFIG
 
 
 class Process:
@@ -8,3 +13,18 @@ class Process:
 
     def run(self):
         raise NotImplementedError
+
+
+class PgProcess:
+    def get_pg_environ(self):
+        url = urlparse(CONFIG['app:main'].get('sqlalchemy.url'))
+        return ChainMap(
+            {
+                'PGHOST': url.hostname,
+                'PGPORT': str(url.port),
+                'PGDATABASE': url.path[1:],
+                'PGPASSWORD': url.password,
+                'PGUSER': url.username,
+            },
+            os.environ
+        )
