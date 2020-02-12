9bfbb8a79fb21525e24b3d415ffe9f9ef76428c9
Simon Mercier <smercier@mapgears.com>
2020-02-12 16:02:18 -0500

    fix add function bash

--- sql/wis/add_function_to_db.sh
+++ sql/wis/add_function_to_db.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
-EXPORT PGPASSWORD=loader
-for i in `./*.sql`; do 
-    psql -h localhost d smtest -U loader -W -f i
+#!/bin/sh
+
+HOST=localhost
+PORT=2345
+DB=sm_adhoc
+USER=loader
+export PGPASSWORD=ChangeMe
+
+for file in *.sql; do 
+    psql -h $HOST -p $PORT -d $DB -U $USER -f $file
+    echo 'psql -h ' $HOST ' -p ' $PORT '-d ' $DB ' -U ' $USER ' -f ' $file
 done
